28d94728b1811347034eba7c6b9035555a9e1021
Keith Potempa <keithrpotempa@gmail.com>
2020-05-18 15:47:03 -0500

    installing react-router-dom

--- src/modules/ApiManager.js
+++ src/modules/ApiManager.js
@@ -2,7 +2,7 @@ const url = "http://localhost:5002"
 
 export default {
   getAll(datatype) {
-    return fetch(`${url}`/${datatype})
+    return fetch(`${url}/${datatype}`)
       .then(result => result.json())
   }
 }
